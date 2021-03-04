681fb09422e955f50cb0a39d3bc5755bbbea5efe
victorbortolotto <victorcamposbortolotto@gmail.com>
2021-03-04 00:57:09 -0300

    Primeira classe Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,15 @@
 package aplicacao;
 
+import boardgame.*;
+
 public class Programa{
 
     public static void main(String[] args) {
         
+        Posicao posicao = new Posicao(3, 5);
+
+
+
     }
 
 }
